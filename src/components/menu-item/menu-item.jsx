92dee30d090621865e3b92a9da675873220dbb26
k-ashu <52956415+k-ashu@users.noreply.github.com>
2019-07-23 20:31:19 +0530

    Added shop page
    Added shop page which shows products from different categories

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import './menu-item.scss'
+import React from "react";
+import "./menu-item.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({title,imageUrl,size}) => (
-    <div className= {`${size} menu-item`}>
-        <div className='background-image' style={{background : `url(${imageUrl})`}} />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ background: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-)
-export default MenuItem
+  </div>
+);
+export default withRouter(MenuItem);
